8ad0c8acf0b062ba78f8effce6e709cd400d4905
Adidi <adieloz@gmail.com>
2021-03-11 14:32:26 +0200

    added deployment script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t adidi/multi-client:latest -t adidi/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t adidi/multi-server:latest -t adidi/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t adidi/multi-worker:latest -t adidi/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push adidi/multi-client:latest
+docker push adidi/multi-server:latest
+docker push adidi/multi-worker:latest
+
+docker push adidi/multi-client:$SHA
+docker push adidi/multi-server:$SHA
+docker push adidi/multi-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/server-deployment server=adidi/multi-server:$SHA
+kubectl set image deployments/client-deployment server=adidi/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=adidi/multi-worker:$SHA
